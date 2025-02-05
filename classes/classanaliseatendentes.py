8cce2a29cf6f4be2559c1902dec7b45bb5edf8d2
Emanuel Soares <tutasoares17@gmail.com>
2025-02-05 17:34:20 +0000

    alot of things

--- classes/classanaliseatendentes.py
+++ classes/classanaliseatendentes.py
@@ -2,6 +2,7 @@ import pandas as pd
 from utils.funcoes import guardar_analise_csv
 from utils.merge_tabelas import merge_tabelas
 from utils.logger_utils import get_logger
+from datetime import datetime
 
 class AnaliseAtendentes:
     def __init__(self, db_manager):
@@ -13,7 +14,7 @@ class AnaliseAtendentes:
             self.df_merged = merge_tabelas(db_manager)
 
             # Garantir que as colunas necessárias estão no DataFrame
-            required_columns = ["Atendente", "ID Atendente", "Quantidade", "Receita Total", "Loja"]
+            required_columns = ["Atendente", "ID Atendente", "Quantidade", "Receita Total", "Loja", "Data"]
             missing_cols = [col for col in required_columns if col not in self.df_merged.columns]
 
             if missing_cols:
@@ -83,3 +84,36 @@ class AnaliseAtendentes:
             self.logger.error(f"Erro ao calcular ticket médio dos atendentes: {e}")
             return None
 
+    def evolucao_vendas_por_atendente(self):
+        """
+        Calcula a evolução do faturamento ao longo do tempo para cada atendente.
+        :return: DataFrame com evolução das vendas ao longo do tempo.
+        """
+        try:
+            # Garantir que a coluna "Data" está no formato datetime
+            self.df_merged["Data"] = pd.to_datetime(self.df_merged["Data"], errors='coerce')
+
+            # Remover valores nulos após conversão
+            self.df_merged = self.df_merged.dropna(subset=["Data"])
+
+            # Obter o ano mais recente disponível nos dados
+            ano_mais_recente = self.df_merged["Data"].dt.year.max()
+
+            # Filtrar apenas para o ano mais recente
+            df_filtrado = self.df_merged[self.df_merged["Data"].dt.year == ano_mais_recente]
+
+            # Agrupar por Data e Atendente para calcular o faturamento
+            df_vendas_tempo = df_filtrado.groupby(["Data", "Atendente"])["Receita Total"].sum().reset_index()
+
+            #formato ano e mes
+            df_vendas_tempo["Data"] = pd.to_datetime(df_vendas_tempo["Data"]).dt.strftime("%Y-%m")
+
+            # Ordenar os meses em ordem crescente
+            df_vendas_tempo = df_vendas_tempo.sort_values("Data", ascending=True)
+
+            self.logger.info("Evolução de vendas por atendente calculada com sucesso.")
+            return df_vendas_tempo
+
+        except Exception as e:
+            self.logger.error(f"Erro ao calcular evolução de vendas por atendente: {e}")
+            return None
